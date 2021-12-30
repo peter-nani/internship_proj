c47ce5e451159976a043a0ee464c3288aa80d5e3
pvundaty <pvundaty@cisco.com>
2021-12-30 11:16:05 +0530

    Login and sign up added to application.

---
+++ app/models.py
@@ -0,0 +1,19 @@
+from django.db import models
+
+# Create your models here.
+
+#auth, User, UserCreationForm
+#6 models
+
+class Person(models.Model):
+    first_name = models.CharField(max_length=30)
+    last_name = models.CharField(max_length=30)
+    roll_no = models.IntegerField(blank = True, null=True)
+    def __str__(self):
+        return self.first_name
+
+class Students(models.Model):
+    first_name = models.CharField(max_length=30)
+    last_name = models.CharField(max_length=30)
+    roll_no = models.IntegerField()
+
